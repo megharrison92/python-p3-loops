e4b4f23b38de3ac4ed949c61a7ae83b5c2589c15
Megan Harrison <mtharrison@alumni.fullsail.edu>
2023-07-05 11:46:20 -0500

    completed lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    counter = 10
+    while counter > 0:
+        print(counter)
+        counter -= 1
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [num ** 2 for num in int_list]
+
+def fizzbuzz_printer():
+    for num in range(1, 101):
+        print(fizzbuzz(num))  
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    num = 0
+    while num < 100:
+        num = num + 1
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
